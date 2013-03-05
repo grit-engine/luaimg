69e45d218308381e72816bc466287e423a406959
Dave Cunningham <sparkprime@gmail.com>
2013-03-05 00:22:16 +0000

    Fix test

--- luaimg/test/test4_greyscale.lua
+++ luaimg/test/test4_greyscale.lua
@@ -1,3 +1,3 @@
 #!../luaimg -F
 
-open("lena.jpg"):map(1, function(c) return vector3(1,1,1) * (c.x + c.y + c.z)/3 end):save("output_test4.bmp")
+open("lena.jpg"):map(1, function(c) return (c.x + c.y + c.z)/3 end):save("output_test4.bmp")
