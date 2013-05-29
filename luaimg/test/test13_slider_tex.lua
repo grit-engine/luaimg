7bed8acbb5fae9da8586afc77391f850e048dd33
Dave Cunningham <sparkprime@gmail.com>
2013-05-29 02:06:53 +0000

    More work on env cycle editor

--- luaimg/test/test13_slider_tex.lua
+++ luaimg/test/test13_slider_tex.lua
@@ -1,14 +1,15 @@
 #!../luaimg -F 
-local sz = vector2(335, 18)
+local sz = vector2(345, 18)
 
-make(sz, 3, function(pos) return HSVtoRGB(vector3(pos.x/(sz.x-1),1,1)) end):save("slider_hue.png")
+make(sz, 3, function(pos) return HSVtoRGB(vector3(pos.x/(sz.x-1),1,1)) end):save("bg_hue.png")
 
-make(sz, 3, function(pos) return pos.x/(sz.x-1) * vector3(1,1,1) end):save("slider_sat.png")
+make(sz, 3, function(pos) return pos.x/(sz.x-1) * vector3(1,1,1) end):save("bg_sat.png")
 
 local tick_colour = vector3(1,0,0)
 local bg_colour = vector3(0,0,0)
 local fg_colour = vector3(1,1,1)
 
+--[[
 make(sz, 3, function(pos)
     local val = pos.x / (sz.x)
     val = pow(val, 2.2)
@@ -34,6 +35,7 @@ make(sz, 3, function(pos)
     val = val * 10
     return (val % 1) * vector3(1,1,1)
 end):save("slider_val2.png")
+]]
 
 make(sz, 3, function(pos)
     local val = pos.x / sz.x
@@ -44,4 +46,12 @@ make(sz, 3, function(pos)
     else
         return (val / 10) * vector3(1,1,1)
     end
-end):save("slider_val3.png")
+end):save("bg_val.png")
+
+make(sz, 3, function(pos)
+    local val = floor(pos / 6)
+	local ch = (val.x + val.y) % 2 == 1 and vector3(1,1,1) or vector3(0,0,0)
+	local alpha = pos.x / sz.x
+
+	return (1-alpha)*ch + alpha*vector3(0.5, 0.5, 0.5)
+end):save("bg_alpha.png")
