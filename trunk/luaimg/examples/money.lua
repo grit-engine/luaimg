1f71c0532280ee23c23fb09c1bff87734c44713c
Dave Cunningham <sparkprime@gmail.com>
2013-10-16 00:41:48 +0000

    New money script

--- trunk/luaimg/examples/money.lua
+++ trunk/luaimg/examples/money.lua
@@ -1,10 +1,13 @@
 #!../luaimg -F
-img = make(vec(512,512),3,0)
+img = make(vec(1920,1200),3,0)
 --vec4(vec(1,2,3),4)
-money = open("money.png"):map(4,true,function(c)return vec4(c.x,c.y,c.z,1)end)
+money = open("money.png")
 moneys = {}
+local bill_size = money.size / vec(2,4)
 for i=0,359 do
-    moneys[i] = lerp(vec(0.7,0.7,0.65),vec(1,1,1)*0.8,random())*money:rotate(i)
+    local off = vec(random(2)-1, random(4)-1) * bill_size
+    local mask = lerp(vec(0.7,0.7,0.65),vec(1,1,1)*0.8,random())
+    moneys[i] = (money:crop(off, bill_size):rotate(i):scaleBy(vec(1,1)/3, "LANCZOS3"))
 end
 for i=1,10000 do
     local m = moneys[random(359)]
@@ -13,6 +16,7 @@ for i=1,10000 do
 end
 
 --sharpen
-img = 2*img - img:convolveSep(gaussian(3), true, true)
+--img = 2*img - img:convolveSep(gaussian(3), true, true)
 
-img:crop(vec(0,0),vec(2048,2048)):save("moneyimg.png")
+--img:crop(vec(0,0),vec(2048,2048)):save("moneyimg.png")
+img:save("moneyimg.png")
